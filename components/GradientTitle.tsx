49aa1f44d7cf292ade2a42d004d953e04fc0b12c
Kozyrev Vladislav <76097937+dalvine@users.noreply.github.com>
2025-04-13 05:47:09 +0300

    fix: change the gradient to match the layout
    use 70% of text

--- components/GradientTitle.tsx
+++ components/GradientTitle.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useRef } from 'react';
 import classNames from 'classnames';
 
 type TitleHTMLTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
@@ -6,20 +6,35 @@ type TitleHTMLTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 interface GradientTextProps {
     text: string;
     className?: string;
-    component: TitleHTMLTag
+    component: TitleHTMLTag;
 }
 
-const GradientTitle: React.FC<GradientTextProps> = ({ text, className, component: Component }) => {
+const GradientTitle: React.FC<GradientTextProps> = ({
+    text,
+    className,
+    component: Component,
+}) => {
+    const partsText = useMemo<[string, string]>(() => {
+        const totalLength = text.length;
 
+        const eightyPercentIndex = Math.floor(totalLength * 0.7);
+
+        const part1 = text.slice(0, eightyPercentIndex);
+        const part2 = text.slice(eightyPercentIndex);
+
+        return [part1, part2];
+    }, []);
 
     return (
-        <Component
-            className={classNames(
-                'bg-clip-text text-transparent bg-gradient-to-r from-main-3 to-main-0 from-60% overflow-visible',
-                className,
-            )}
-        >
-            {text}
+        <Component className={className}>
+            {partsText[0]}
+            <span
+                className={
+                    'bg-clip-text text-transparent bg-gradient-to-r from-main-2 to-main-0 overflow-visible'
+                }
+            >
+                {partsText[1]}
+            </span>
         </Component>
     );
 };
